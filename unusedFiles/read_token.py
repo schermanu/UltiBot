c35cc4fa80c2ba42986e81131b4b833ce5308cf5
schermanu <schermanu@gmail.com>
2021-12-31 01:59:46 +0100

    nettoyage

--- unusedFiles/read_token.py
+++
@@ -1,5 +0,0 @@
-# Read the bot token contained in the file located at the given path.
-def read_bot_token(filePath):
-    with open(filePath, "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
