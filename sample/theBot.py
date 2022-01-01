4ff4998a86a0102d15161984d3eceaf4fb3d9ec9
schermanu <schermanu@gmail.com>
2022-01-01 19:29:31 +0100

    add "BotParameters" class to handle config and states messages on discord config_channel

--- sample/theBot.py
+++ sample/theBot.py
@@ -3,7 +3,6 @@ import configparser
 import datetime
 import asyncio
 
-import discord
 from discord.ext import commands
 from bot_commands import load_commands
 import constants as CST
@@ -14,10 +13,7 @@ import constants as CST
 class TheBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or('?'), case_insensitive=False)
-
-        self.state = ConfigurationParser()
-        self.config = ConfigurationParser()
-        self.paramMessages = {}
+        self.param = BotParameters()
         self.routines = []
         self.routinesTask = None
         self.routinesTaskStartTime = None
@@ -117,42 +113,22 @@ class TheBot(commands.Bot):
     def log(self, msg):
         print(f"[bot] {msg}")
 
-    async def load_param_msgs(self):
-        allMsg = await self.get_channel(CST.CONFIG_CHANNEL_ID).history().flatten()
-        # botMessages = [msg for msg in allMsg if msg.author == self.user]
-        # notBotMessage = [msg for msg in allMsg if msg not in botMessages]
-        botStateStr = ""
-        botConfigStr = ""
-        for msg in allMsg:
-            if msg.content.startswith("#**state**#"):
-                self.paramMessages["state"] = msg
-                botStateStr = msg.content
-                continue
-        for msg in allMsg:
-            if msg.content.startswith("#**config**#"):
-                self.paramMessages["config"] = msg
-                botConfigStr = msg.content
-                continue
-        print(f"state:\n{botStateStr}\n")
-        print(f"config:\n{botConfigStr}")
-        return botStateStr, botConfigStr
-
     async def load_state_and_config(self):
-        botStateStr, botConfigStr = await self.load_param_msgs()
-        self.state.read_string(botStateStr)
-        self.config.read_string(botConfigStr)
+        botStateStr, botConfigStr = await self.param.load_param_msgs(self)
+        self.param.state.read_string(botStateStr)
+        self.param.config.read_string(botConfigStr)
 
-        if self.state.has_section('bot'):
-            botConfig = self.state['bot']
+        if self.param.state.has_section('bot'):
+            botConfig = self.param.state['bot']
             self.routinesTriggerTime = botConfig.gettime('routinesTriggerTime')
             self.lastRoutinesTriggerDate = botConfig.getdatetime('lastRoutinesTriggerDate')
 
             for routine in self.routines:
-                routine.load_routines_state(self.state)
+                routine.load_routines_state(self.param.state)
 
     async def save_state(self):
 
-        self.state['bot'] = \
+        self.param.state['bot'] = \
             {
                 'routinesTriggerTime':
                     "" if self.routinesTriggerTime is None
@@ -163,28 +139,8 @@ class TheBot(commands.Bot):
             }
 
         for routine in self.routines:
-            routine.save_routines_state(self.state)
-        await self.write_state()
-
-    async def write_state(self):
-
-        buf = io.StringIO("")
-        self.state.write(buf)  # utilisation d'un buffer car la methode write() de configParser ne permet pas d'ecrire
-        # dans une string
-        strToWrite = buf.getvalue()
-        buf.close()
-        header = "#**state**#\n#sauvegarde de l'état du bot\n"
-        stateMsg = self.paramMessages["state"]
-        if stateMsg is None:
-            self.paramMessages["state"] = await self.get_channel(CST.CONFIG_CHANNEL_ID).send(header + strToWrite)
-
-        elif stateMsg.author == self.user:
-            await stateMsg.edit(header + strToWrite)
-        else:
-            await stateMsg.delete()
-            self.paramMessages["state"] = await self.get_channel(CST.CONFIG_CHANNEL_ID).send(header + strToWrite)
-
-        # need to remove other ?
+            routine.save_routines_state(self.param.state)
+        await self.param.write_state(self)
 
 
 # Parser of the bot's state. Allows to save and restore its state after a reboot.
@@ -217,3 +173,54 @@ class ConfigurationParser(configparser.ConfigParser):
         except (ValueError, TypeError):
             return None
 
+
+class BotParameters:
+    def __init__(self):
+        self.state = ConfigurationParser()
+        self.config = ConfigurationParser()
+        self.paramMessages = {}
+        self.stateHeader = ""
+        self.configHeader = ""
+
+    async def write_state(self, bot):
+
+        buf = io.StringIO("")
+        self.state.write(
+            buf)  # utilisation d'un buffer car la methode write() de configParser ne permet pas d'ecrire
+        # dans une string
+        strToWrite = buf.getvalue()
+        buf.close()
+        stateMsg = self.paramMessages["state"]
+        if stateMsg is None:
+            self.paramMessages["state"] = await bot.get_channel(CST.CONFIG_CHANNEL_ID).send(
+                self.stateHeader + strToWrite)
+
+        elif stateMsg.author == bot.user:
+            await stateMsg.edit(self.stateHeader + strToWrite)
+        else:
+            await stateMsg.delete()
+            self.paramMessages["state"] = await bot.get_channel(CST.CONFIG_CHANNEL_ID).send(
+                self.stateHeader + strToWrite)
+        # need to remove other messages?
+
+    async def load_param_msgs(self, bot):
+        allMsg = await bot.get_channel(CST.CONFIG_CHANNEL_ID).history().flatten()
+        # botMessages = [msg for msg in allMsg if msg.author == self.user]
+        # notBotMessage = [msg for msg in allMsg if msg not in botMessages]
+        botStateStr = ""
+        botConfigStr = ""
+        for msg in allMsg:
+            # check only the firsts 7 characters to avoid checking the comments
+            if msg.content.startswith(self.stateHeader[0:6]):
+                self.paramMessages["state"] = msg
+                botStateStr = msg.content
+                break
+        for msg in allMsg:
+            # the header doesn't contain comments here
+            if msg.content.startswith(self.configHeader):
+                self.paramMessages["config"] = msg
+                botConfigStr = msg.content
+                break
+        print(f"state:\n{botStateStr}\n")
+        print(f"config:\n{botConfigStr}")
+        return botStateStr, botConfigStr
